90c9021eaa1aba73c91618c6199a929043bfc40c
Hyun-h <hyeonahong@gmail.com>
2021-11-28 16:37:29 +0900

    img 파일 반복 되는거 map함수로 사용해봄

--- src/Product.js
+++ src/Product.js
@@ -5,10 +5,18 @@ import React from "react";
 function Product(props) {
   return (
     <>
-      {props.shoes.map((data) => {
+      {props.shoes.map((data, index) => {
         return (
-          <div className="col-md-4" kye={data.id}>
-            <img src={data.img} alt="shoes" width="100%" />
+          <div className="col-md-4" kye={index}>
+            <img
+              src={
+                "https://codingapple1.github.io/shop/shoes" +
+                (index + 1) +
+                ".jpg"
+              }
+              alt="shoes"
+              width="100%"
+            />
             <h4>{data.title}</h4>
             <span>{data.price}</span>
             <p>{data.content}</p>
